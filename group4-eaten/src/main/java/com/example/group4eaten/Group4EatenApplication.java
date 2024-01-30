b63e6482aab856dd36cd4632e6153865656261be
hwakyong YOO <hwakyongyoo@gmail.com>
2024-01-30 14:35:22 +0900

    feat-초기 프로젝트 설정

---
+++ group4-eaten/src/main/java/com/example/group4eaten/Group4EatenApplication.java
@@ -0,0 +1,13 @@
+package com.example.group4eaten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group4EatenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group4EatenApplication.class, args);
+	}
+
+}
---
+++ group4-eaten/src/test/java/com/example/group4eaten/Group4EatenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.group4eaten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group4EatenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
