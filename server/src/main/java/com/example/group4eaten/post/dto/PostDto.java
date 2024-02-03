fe86d7ee03d42e4cf7a569e774ed60b66f32101f
yoonhyung <pyoon820@gmail.com>
2024-02-03 22:00:59 +0900

    feat-게시물 수정, 게시물 삭제 / fix-게시물 생성 request body 수정

--- server/src/main/java/com/example/group4eaten/post/dto/PostDto.java
+++ server/src/main/java/com/example/group4eaten/post/dto/PostDto.java
@@ -2,10 +2,7 @@ package com.example.group4eaten.post.dto;
 
 import com.example.group4eaten.entity.Post;
 import com.example.group4eaten.entity.User; // User 엔터티 임포트
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -21,6 +18,7 @@ public class PostDto {
     private String imagepath;
     private String edit_YN;
 
+    //게시물 조회할 때 사용 - 기존에 저장된 Post 엔티티를 PostDto로 변환
     public static PostDto createPostDto(Post post) {
         return new PostDto(
                 post.getPostId(),
@@ -32,6 +30,24 @@ public class PostDto {
         );
     }
 
+    //생성자를 통한 게시물 생성 - 객체를 생성하면서 필요한 필드만 선택적으로 초기화
+    @Builder
+    public PostDto(String userId, String content, String imagepath) {
+        this.userId = userId;
+        this.content = content;
+        this.date = LocalDate.now();
+        this.imagepath = imagepath;
+    }
+
+    //게시물 수정 - 주어진 파라미터로 객체의 상태를 업데이트
+    public void updatePostDto(String userId, String content, String imagepath){
+        this.userId = userId;
+        this.content = content;
+        this.imagepath = imagepath;
+    }
+
+
+    //PostDto를 Post 엔티티로 변환
     public Post toEntity(User user) {
         return new Post(postId, user, content, date, imagepath, edit_YN);
     }
