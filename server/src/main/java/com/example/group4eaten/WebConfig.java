836b7ec013f6394eae2ace73a15d50e191d4ee24
hwakyong YOO <hwakyongyoo@gmail.com>
2024-02-21 20:58:23 +0900

    [BE] fix: webcofig corsfilter 설정

--- server/src/main/java/com/example/group4eaten/WebConfig.java
+++ server/src/main/java/com/example/group4eaten/WebConfig.java
@@ -1,8 +1,13 @@
 package com.example.group4eaten;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -28,4 +33,21 @@ public class WebConfig implements WebMvcConfigurer {
         // .allowedHeaders("header1", "header2") // 필요한 경우, 추가적인 허용 헤더
         // .exposedHeaders("header1", "header2") // 필요한 경우, 노출할 헤더
     }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(6000L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new CorsFilter(source));
+        filterBean.setOrder(0);
+
+        return filterBean;
+    }
+
 }
