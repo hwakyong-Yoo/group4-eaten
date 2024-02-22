92ac949a5fc2550d29be76336a789f91dff428d2
Hanrann6 <yookrann@ewhain.net>
2024-02-22 21:28:22 +0900

    [FE] fix: hello xhr 속성 제거

--- client/src/api/hello.ts
+++ client/src/api/hello.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import { API } from './api.const';
 axios.defaults.withCredentials = true;
-const xhr = new XMLHttpRequest();
-xhr.open('GET', `/api/hello`, true);
-xhr.withCredentials = true;
+// const xhr = new XMLHttpRequest();
+// xhr.open('GET', `/api/hello`, true);
+// xhr.withCredentials = true;
 
 // api/hello 엔드포인트로 GET 요청을 보내는 함수
 const fetchHelloData = async () => {
