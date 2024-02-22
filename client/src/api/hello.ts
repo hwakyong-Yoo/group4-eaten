ebfe90e9662344051adbde9cbc915f37b0db441e
Hanrann6 <yookrann@ewhain.net>
2024-02-22 21:35:25 +0900

    [FE] fix: hello axios 코드 수정

--- client/src/api/hello.ts
+++ client/src/api/hello.ts
@@ -1,17 +1,14 @@
 import axios from 'axios';
 import { API } from './api.const';
 axios.defaults.withCredentials = true;
-// const xhr = new XMLHttpRequest();
-// xhr.open('GET', `/api/hello`, true);
-// xhr.withCredentials = true;
 
 // api/hello 엔드포인트로 GET 요청을 보내는 함수
 const fetchHelloData = async () => {
   try {
     // GET 요청 보내기
+    axios.defaults.withCredentials = true;
     const response = await axios.get(`/api/hello`, {
       headers: {
-        Accept: 'application/json',
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': 'https://eaten-ecc.site',
       },
