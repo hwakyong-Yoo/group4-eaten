7e0e3276a0e0f4f026d671097a34bc138f8ac7c8
yoonhyung <pyoon820@gmail.com>
2024-03-08 14:00:52 +0900

    [BE]feat: corsorigin에 localhost:3000 추가

--- server/src/main/java/com/example/group4eaten/CorsConfig.java
+++ server/src/main/java/com/example/group4eaten/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("https://eaten-five.vercel.app");
+        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         config.setAllowCredentials(true);
